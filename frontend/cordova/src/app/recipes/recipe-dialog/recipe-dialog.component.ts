c5da1431a1ef31931957cb777fea5fe58257b5a2
TheRicoSenpai <bourteau.nicolas@gmail.com>
2020-09-24 20:37:38 +0200

    Refactoring of recipe.dialog.compoenent

--- frontend/cordova/src/app/recipes/recipe-dialog/recipe-dialog.component.ts
+++ frontend/cordova/src/app/recipes/recipe-dialog/recipe-dialog.component.ts
@@ -5,12 +5,15 @@ import { RecipeType } from '../../model/recipe-type.enum';
 import { Ingredient, Recipe, Unit } from '../../model';
 import {IngredientDialogComponent} from '../../ingredients/ingredient-dialog/ingredient-dialog.component';
 import { DialogComponent } from '../../core/shared/component/dialog/dialog.component';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { IngredientHelper } from '../../core/shared/helpers/ingredient/ingredient-helper';
 import { RecipeService } from '../../core/shared/service/data/recipe.service';
 import { IngredientComparer } from '../../core/shared/helpers/ingredient/ingredient-comparer';
 import { QuantityConverter } from 'src/app/core/shared/helpers/units/unit-converter';
-
+interface IngredientItemDetail {
+  ingredient: Ingredient;
+  detailVisible: boolean;
+}
 @Component({
   selector: 'app-recipe-dialog',
   templateUrl: './recipe-dialog.component.html',
@@ -20,6 +23,15 @@ export class RecipeDialogComponent extends DialogComponent implements OnInit {
   recipeTypes: string[] = Object.keys(RecipeType).map(key => RecipeType[key]);
   currentData: Recipe;
   form: FormGroup;
+  ingFormArray: FormArray;
+  ingForm: FormGroup;
+  itemDetail: IngredientItemDetail = {
+    ingredient: new Ingredient(),
+    detailVisible: false
+  };
+  lastIIDVisible: IngredientItemDetail;
+  newDirectionVisible: boolean;
+  newDirection = this.fb.control('');
   color = 'primary';
 
   constructor(private recipesService: RecipeService,
@@ -33,6 +45,7 @@ export class RecipeDialogComponent extends DialogComponent implements OnInit {
         if  (result)
         {
           this.currentData = result;
+          this.ingFormArray = this.fb.array(this.currentData.ingredients.map(value => ({ ingredient : value, detailVisible: false }) ));
           this.form = this.fb.group({
             name: this.currentData.name,
             cookingTime : this.currentData.cookingTime,
@@ -40,7 +53,7 @@ export class RecipeDialogComponent extends DialogComponent implements OnInit {
             numberOfPeople : this.currentData.numberOfPeople,
             favorite : this.currentData.favorite,
             photoPath : this.currentData.photoPath,
-            ingredients : this.fb.array(this.currentData.ingredients),
+            ingredients : this.ingFormArray,
             directions : this.fb.array(this.currentData.directions),
             hashtags : this.fb.array(this.currentData.hashtags)
           });
@@ -49,7 +62,7 @@ export class RecipeDialogComponent extends DialogComponent implements OnInit {
       this.currentData = {...oData};
       this.currentData.ingredients = this.currentData.ingredients
         .map(i => new Ingredient(i.quantity, i.unit, i.type, i.foodType, i.favorite));
-
+      this.ingFormArray =  this.fb.array(this.currentData.ingredients.map(value => ({ ingredient : value, detailVisible: false }) ));
       this.form = this.fb.group({
         name: this.currentData.name,
         cookingTime : this.currentData.cookingTime,
@@ -57,10 +70,11 @@ export class RecipeDialogComponent extends DialogComponent implements OnInit {
         numberOfPeople : this.currentData.numberOfPeople,
         favorite : this.currentData.favorite,
         photoPath : this.currentData.photoPath,
-        ingredients : this.fb.array(this.currentData.ingredients),
+        ingredients : this.ingFormArray,
         directions : this.fb.array(this.currentData.directions),
         hashtags : this.fb.array(this.currentData.hashtags)
       });
+      this.initIngregientFormGroup();
     }
 
   ngOnInit() {
@@ -77,7 +91,7 @@ export class RecipeDialogComponent extends DialogComponent implements OnInit {
     this.currentData.numberOfPeople = this.form.controls.numberOfPeople.value;
     this.currentData.favorite = this.form.controls.favorite.value;
     this.currentData.photoPath = this.form.controls.photoPath.value;
-    this.currentData.ingredients = this.form.controls.ingredients.value;
+    this.currentData.ingredients = this.form.controls.ingredients.value.map(value => value.ingredient);
     this.currentData.directions = this.form.controls.directions.value;
     this.currentData.hashtags = this.form.controls.hashtags.value;
   }
@@ -119,6 +133,46 @@ export class RecipeDialogComponent extends DialogComponent implements OnInit {
   removeIngredient(idx: number){
     this.form.controls.ingredients.value.splice(idx, 1);
   }
+  modifyItem(iid: IngredientItemDetail) {
+    console.log('iid :' + iid);
+    this.setDetailVisible(iid, true);
+  }
+  addItem(iid: IngredientItemDetail) {
+    const newIngredient = new Ingredient(
+      this.ingForm.controls.quantity.value,
+      this.ingForm.controls.unit.value,
+      this.ingForm.controls.name.value,
+      this.ingForm.controls.foodType.value,
+      false);
+    this.ingFormArray.push(this.fb.control(({ingredient: newIngredient, detailVisible: false })));
+    this.setDetailVisible(iid, false);
+  }
+  updateItem(iid: IngredientItemDetail){
+    iid.ingredient.quantity = this.ingForm.controls.quantity.value;
+    iid.ingredient.unit = this.ingForm.controls.unit.value;
+    iid.ingredient.type = this.ingForm.controls.name.value;
+    iid.ingredient.foodType = this.ingForm.controls.foodType.value;
+    this.setDetailVisible(iid, false);
+  }
+  cancelItem(iid: IngredientItemDetail) {
+    this.setDetailVisible(iid, false);
+  }
+  setDetailVisible(iid: IngredientItemDetail, isVisible: boolean){
+    if (this.lastIIDVisible){
+      this.lastIIDVisible.detailVisible = false;
+    }
+    iid.detailVisible = isVisible;
+    this.initIngregientFormGroup(isVisible ? iid : undefined);
+    this.lastIIDVisible = iid;
+  }
+  initIngregientFormGroup(iid?: IngredientItemDetail){
+    this.ingForm = this.fb.group({
+      name: iid?.ingredient.type ?? '',
+      quantity: iid?.ingredient.quantity ?? undefined,
+      unit: iid?.ingredient.unit ?? '',
+      foodType: iid?.ingredient.foodType ?? ''
+    });
+  }
 //#endregion
 
 //#region  Directions
@@ -127,11 +181,17 @@ export class RecipeDialogComponent extends DialogComponent implements OnInit {
   }
 
   addDirection(){
-    this.form.controls.directions.value.push('');
+    this.form.controls.directions.value.push(this.newDirection.value);
+    this.newDirectionVisible = false,
+    this.newDirection.reset();
   }
 
   removeDirection(idx: number){
     this.form.controls.directions.value.splice(idx, 1);
+  }
+  cancelNewDirection() {
+    this.newDirectionVisible = false,
+    this.newDirection.reset();
   }
 //#endregion
 
